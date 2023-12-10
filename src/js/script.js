3f49122a456c78fbf88b42f6d475fbed07349cb2
Artem Tumanov home <artem.tumanov.93@inbox.ru>
2023-12-10 19:45:24 +0300

    add new sections 10.12.2023

---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('.slider__wrapper').slick({
+        speed: 1000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/right.png"></button>'
+    });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__wrapper').removeClass('catalog__wrapper_active').eq($(this).index()).addClass('catalog__wrapper_active');
+      });
+
+      function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__front').eq(i).toggleClass('catalog-item__front_active');
+                $('.catalog-item__back').eq(i).toggleClass('catalog-item__back_active');
+            })
+        })
+      }
+
+      toggleSlide('.catalog-item__link');
+      toggleSlide('.catalog-item__return');
+  });
