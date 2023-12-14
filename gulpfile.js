2f9837dfe1a1e523e0534c677b84a123e3a2c870
Artem Tumanov <artem.tumanov.93@inbox.ru>
2023-12-14 15:36:02 +0300

    end Pulse 14.12.2023

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,14 @@ const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
 const cleanCSS = require('gulp-clean-css');
+const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
 
 // Static server
 gulp.task('server', function() {
     browserSync({
         server: {
-            baseDir: "src"
+            baseDir: "dist"
         }
     });
 	gulp.watch("src/*.html").on('change', browserSync.reload);
@@ -21,12 +23,40 @@ gulp.task('styles', function() {
         .pipe(rename({prefix: "", suffix: ".min"}))
 		.pipe(cleanCSS({compatibility: 'ie8'}))
 		.pipe(autoprefixer())
-        .pipe(gulp.dest('src/css'))
+        .pipe(gulp.dest('dist/css'))
         .pipe(browserSync.stream());
 })
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
-})
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel("styles"));
+    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+});
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('html', function () {
+    return gulp.src("src/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src("src/js/**/*")
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('fonts', function () {
+    return gulp.src("src/fonts/**/*")
+        .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('icons', function () {
+    return gulp.src("src/icons/**/*")
+        .pipe(gulp.dest('dist/icons'));
+});
+
+gulp.task('images', function () {
+    return gulp.src("src/img/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'html',  'images'));
